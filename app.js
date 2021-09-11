d88218c3da1a151a5d97140d8af258b53f6fd21f
shikhar0512L <74178918+shikhar0512L@users.noreply.github.com>
2021-09-11 10:43:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+var dt = new Date();
+
+console.log(dt);
+
+// IMPORTANT
+ 
+function hideFunction() {
+    var alert = document.getElementById("alert");
+    alert.style.display = "block";
+    return true;
+};
+
+const obj =  {
+    name:"Shikhar",
+    class:8,
+    sec:'c',
+    rollNo:35
+};
+
+console.log(obj);
